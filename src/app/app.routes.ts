fa426d41c02cb5db5cab0887d6a3cf9d739fe354
beatrizlima-tech <bealima.tech@gmail.com>
2026-06-24 13:48:30 -0300

    feat: implementa autenticação de usuários e dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AutenticarUsuario } from './autenticar-usuario/autenticar-usuario';
+import { CriarUsuario } from './criar-usuario/criar-usuario';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pages/autenticar', //Rota de navegação
+        component: AutenticarUsuario //Componente renderizado
+        
+    },
+    {
+        path: 'pages/criar-usuario', //Rota de navegação
+        component: CriarUsuario //Componente renderizado
+    },
+    {
+        path: 'app/dashboard', //Rota de navegação
+        component: Dashboard //Componente renderizado
+    },
+    {
+        path: '', pathMatch: 'full', //Rota raiz
+        redirectTo: '/pages/autenticar' //Redurecionamento
+    }
+];
